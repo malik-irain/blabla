169ac6fff72bf843cf2271dd40cfe0e767b036f9
Malik Irain <malik.irain@cemes.fr>
2025-01-14 14:53:49 +0100

    oui

--- child.py
+++ child.py
@@ -4,11 +4,11 @@ import subprocess
 import sys
 
 if __name__ == "__main__":
-	print(f'Child\'s parent PID: {os.getppid()}')
+	print(f'Child\'s parent PID: {os.getpid()}, {os.getppid()}')
 	try:
 		sys.stdin.read()
-		time.sleep(10)
-		print(f'Child\'s parent PID: {os.getppid()}')
+		time.sleep(60)
+		print(f'Child\'s parent PID: {os.getpid()}, {os.getppid()}')
 	except e:
 		print(f"Couldn't wait parent: {e}")
 
